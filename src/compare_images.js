3f4e11dd0fa8324bd99d36f57836db8490e59865
klawr <mail@kailawrence.de>
2021-07-07 14:56:59 +0200

    Start the finish of the Pendel...

--- src/compare_images.js
+++ src/compare_images.js
@@ -1,4 +1,12 @@
 
+/**
+ * 
+ * @param {*} image1 The first image
+ * @param {*} image2 The second image
+ * @param {*} width Width of the images (have to be equal)
+ * @param {*} height Height of the images (have to be equal)
+ * @returns 
+ */
 function compare_images(image1, image2, width, height) {
     const difference = [];
 
