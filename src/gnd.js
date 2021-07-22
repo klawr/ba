d040aca2ad00c284d2ba7f16f9a7b487c177694e
klawr <mail@kailawrence.de>
2021-07-22 15:50:41 +0200

    Add interesting tests to pendel1

--- src/gnd.js
+++ src/gnd.js
@@ -19,8 +19,13 @@ class Gnd {
         this.data.y[y] = this.data.y[y] ? this.data.y[y] + 1 : 1;
     };
     getChart() {
-        const x = Object.entries(this.data.x).flatMap(e => [+e[0], e[1]]);
-        const y = Object.entries(this.data.y).flatMap(e => [+e[0], e[1]]);
+        const align = (arr, limit) => Object.entries(arr)
+            .filter(e => limit ? (+e[0] > 0 && +e[0] < limit) : true)
+            .sort((a, b) => +a[0] > +b[0])
+            .flatMap(e => [+e[0], e[1]]);
+
+        const x = align(this.data.x, 320); // Canvas width
+        const y = align(this.data.y, 180); // Canvas height
 
         return g2().clr().view({ cartesian: true }).chart({
             x: 20, y: 20, b: 280, h: 150,
@@ -31,7 +36,9 @@ class Gnd {
     }
 
     add(a, threshold) {
-        this.addDataPoint(a);
+        if (!this.confident) {
+            this.addDataPoint(a);
+        }
         if (this.past.push(a) > 10) {
             this.past.shift();
         };
