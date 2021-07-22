b1fdcc140226129d2f222fe1719bb4850be6f3f9
klawr <mail@kailawrence.de>
2021-07-22 13:20:57 +0200

    Implementation of uniform measures + renaming

--- src/gnd.js
+++ src/gnd.js
@@ -8,7 +8,30 @@ class Gnd {
         return this.past[this.past.length - 1];
     }
 
+    data = {
+        x: {},
+        y: {},
+    };
+    addDataPoint(a) {
+        const x = Math.round(a.x);
+        const y = Math.round(a.y);
+        this.data.x[x] = this.data.x[x] ? this.data.x[x] + 1 : 1;
+        this.data.y[y] = this.data.y[y] ? this.data.y[y] + 1 : 1;
+    };
+    getChart() {
+        const x = Object.entries(this.data.x).flatMap(e => [+e[0], e[1]]);
+        const y = Object.entries(this.data.y).flatMap(e => [+e[0], e[1]]);
+
+        return g2().clr().view({ cartesian: true }).chart({
+            x: 20, y: 20, b: 280, h: 150,
+            funcs: [{ data: x }, { data: y }],
+            xaxis: {},
+            yaxis: {},
+        });
+    }
+
     add(a, threshold) {
+        this.addDataPoint(a);
         if (this.past.push(a) > 10) {
             this.past.shift();
         };
