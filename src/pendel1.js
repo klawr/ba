5f4f83801c313f2ef75664bb9ac18417d6edfbc4
Kai Lawrence <mail@kailawrence.de>
2021-07-23 17:29:53 +0200

    Add addPointForCircle to pendel1

--- src/pendel1.js
+++ src/pendel1.js
@@ -4,3 +4,22 @@ reset = () => {
     ply = [];
 };
 let nod; // Endstück des Pendels
+
+function addPointForCircle(result) {
+    if (!gnd.confident) {
+        result.forEach(r => {
+            if (!gnd.confident) {
+                // Draw a circle for every found change
+                g2().cir({ ...r, r: 1 }).exe(ctx2);
+                ply.push({ ...r });
+            }
+        });
+    }
+
+    const cir = makeCircle(ply);
+    ply.length && gnd.add(cir);
+
+    g2().cir({ ...gnd.past[gnd.past.length - 1] }).exe(ctx2);
+
+    return cir;
+}
