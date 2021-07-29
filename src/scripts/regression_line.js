f648a5f5189408f5938e2abd8e34925f35114757
klawr <mail@kailawrence.de>
2021-07-29 21:08:45 +0200

    Move regression_line into separate file

---
+++ src/scripts/regression_line.js
@@ -0,0 +1,45 @@
+
+function regression_line(pts) {
+    let max = 0;
+    let p1;
+    let p2;
+
+    pts.forEach(r => {
+        pts.forEach(s => {
+            const hy = Math.hypot(r.y - s.y, r.x - s.x);
+            if (hy > max) {
+                max = hy;
+                if (r.x > s.x) {
+                    p1 = s;
+                    p2 = r;
+                } else {
+                    p1 = r;
+                    p2 = s;
+                }
+            };
+        });
+    });
+
+    if (!p1 || !p2) {
+        return;
+    }
+
+    const tmp = (p2.y - p1.y) / (p2.x - p1.x);
+    const swap = Math.abs(Math.atan(tmp)) < Math.PI / 4;
+
+    const n = pts.length;
+    if (!n) return;
+    const sxy = pts.map(e => e.x * e.y).reduce((pre, cur) => cur + pre);
+    const sx = pts.map(e => e.x).reduce((pre, cur) => cur + pre);
+    const sy = pts.map(e => e.y).reduce((pre, cur) => cur + pre);
+
+    const s2 = pts.map(e => (swap ? e.x : e.y) ** 2).reduce((pre, cur) => cur + pre);
+
+    const counter = (n * sxy - sx * sy);
+    const denominator = (n * s2 - (swap ? sx : sy) ** 2);
+
+    let m = swap ? counter / denominator : denominator / counter;
+    let b = (sy - m * sx) / n;
+
+    return { m, b };
+}
--- src/scripts/removeOverlaps.js
+++ src/scripts/removeOverlaps.js
@@ -1,13 +1,13 @@
 
-function removeOverlaps(boxes) {
+function removeOverlaps(boxes, dist = 5) {
     const mostAccurate = [];
     while (boxes.length) {
         const max = boxes.pop();
         mostAccurate.push(max);
         boxes = boxes.filter(
             (rec) =>
-                Math.abs(rec.x - max.x) >= 10 ||
-                Math.abs(rec.y - max.y) >= 10
+                Math.abs(rec.x - max.x) >= dist ||
+                Math.abs(rec.y - max.y) >= dist
         );
     }
     return mostAccurate;
