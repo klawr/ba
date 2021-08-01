67e1d76a549db2d14548aafee8189d13249b190e
klawr <mail@kailawrence.de>
2021-08-01 21:18:34 +0200

    Move regressionLine into Line class

--- src/scripts/line.js
+++ src/scripts/line.js
@@ -31,6 +31,58 @@ class Line {
         return { x, y };
     }
 
+    static fromRegressionLine(pts, g) {
+        let max = 0;
+        let p1;
+        let p2;
+
+        pts.forEach(r => {
+            pts.forEach(s => {
+                const hy = Math.hypot(r.y - s.y, r.x - s.x);
+                if (hy > max) {
+                    max = hy;
+                    if (r.x > s.x) {
+                        p1 = s;
+                        p2 = r;
+                    } else {
+                        p1 = r;
+                        p2 = s;
+                    }
+                };
+            });
+        });
+
+        if (!p1 || !p2) {
+            return;
+        }
+
+        const tmp = (p2.y - p1.y) / (p2.x - p1.x);
+        const swap = Math.abs(Math.atan(tmp)) < Math.PI / 4;
+
+        const n = pts.length;
+        if (!n) return;
+        const sxy = pts.map(e => e.x * e.y).reduce((pre, cur) => cur + pre);
+        const sx = pts.map(e => e.x).reduce((pre, cur) => cur + pre);
+        const sy = pts.map(e => e.y).reduce((pre, cur) => cur + pre);
+
+        const s2 = pts.map(e => (swap ? e.x : e.y) ** 2).reduce((pre, cur) => cur + pre);
+
+        const counter = (n * sxy - sx * sy);
+        const denominator = (n * s2 - (swap ? sx : sy) ** 2);
+
+        let m = swap ? counter / denominator : denominator / counter;
+        let b = (sy - m * sx) / n;
+
+        g?.lin({
+            x1: 0,
+            x2: globalTestVariables.cnv_width,
+            y1: b,
+            y2: m * globalTestVariables.cnv_width + b
+        });
+
+        return new Line({ m, b });
+    }
+
     static fromBisector(p1, p2, g) {
         const mid = {
             x: (p1.x + p2.x) / 2,
@@ -40,9 +92,9 @@ class Line {
         const m = 1 / ((p1.y - p2.y) / (p2.x - p1.x));
         const b = mid.y - m * mid.x;
 
-        const line = new Line({m, b});
+        const line = new Line({ m, b });
 
-        g?.lin({p1, p2});
+        g?.lin({ p1, p2 });
         g && line.draw(g);
 
         return line;
