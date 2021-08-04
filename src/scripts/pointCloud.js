90673188b89cac52488960ef4ab5141b30277945
klawr <mail@kailawrence.de>
2021-08-04 20:08:26 +0200

    Fix getMaxDist

--- src/scripts/pointCloud.js
+++ src/scripts/pointCloud.js
@@ -61,69 +61,45 @@ class PointCloud {
      * @returns 
      */
     getMaxDist(g, number = 1) {
-        let copy = [...this.points];
-
-        const p1s = [];
-        const p2s = [];
-
-        for (let idx = 0; idx < number; ++idx) {
-            let max = 0;
-            let i1 = 0;
-            let j1 = 0;
-            let p1;
-            let p2;
-            for (let i = 0; i < copy.length; ++i) {
-                for (let j = 1; j < copy.length; ++j) {
-                    const r = copy[i];
-                    const s = copy[j];
-                    const hy = Math.hypot(r.y - s.y, r.x - s.x);
-
-                    if (hy > max) {
-                        max = hy;
-                        i1 = i;
-                        j1 = j;
-                        p1 = s;
-                        p2 = r;
-                    }
-                }
+        let p1;
+        let p2;
+        let max = 0;
+        for (let i = 0; i < this.points.length; ++i) {
+            for (let j = i + 1; j < this.points.length; ++j) {
+                const r = this.points[i];
+                const s = this.points[j];
+                const hy = Math.hypot(r.y - s.y, r.x - s.x);
+                if (hy > max) {
+                    max = hy;
+                    p1 = s;
+                    p2 = r;
+                };
             }
-
-            if (!p1 || !p2) continue;
-
-            const oneToOne = p1s.length &&
-                Math.hypot(p1s[0].y - p1.y, p1s[0].x - p1.x) <
-                Math.hypot(p2s[0].y - p2.y, p2s[0].x - p2.x);
-
-            if (oneToOne) {
-                p1s.push(p1);
-                p2s.push(p2);
-            } else {
-                p1s.push(p2);
-                p2s.push(p1);
-            }
-
-            copy = copy.splice(i1, 1).splice(j1, 1);
         }
 
-        function cog(arr) {
-            let p = arr.reduce((pre, cur) => ({
-                x: pre.x + cur.x,
-                y: pre.y + cur.y,
-            }), { x: 0, y: 0 });
-            p.x /= arr.length;
-            p.y /= arr.length;
-
-            return p;
+        if (number > 1) {
+            const getDist = p => (a, b) =>
+                Math.hypot(p.y - a.y, p.x - a.x) -
+                Math.hypot(p.y - b.y, p.x - b.x);
+            const cog = (pre, cur) => ({y: pre.y + cur.y, x: pre.x + cur.x});
+            console.log()
+            p1 = [...this.points]
+                .sort(getDist(p1))
+                .slice(0, number)
+                .reduce(cog);
+            p1.x /= number;
+            p1.y /= number;
+            p2 = [...this.points]
+                .sort(getDist(p2))
+                .slice(0, number)
+                .reduce(cog);
+            p2.x /= number;
+            p2.y /= number;
         }
-
-        const p1 = cog(p1s);
-        const p2 = cog(p2s);
-        const max = Math.hypot(p1.y - p2.y, p1.x - p2.x);
-
         g?.cir({ ...p1, r: 5, fs: 'red' }).cir({ ...p2, r: 5, fs: 'red' });
 
         // TODO wird max je genutzt?
-        return { p1, p2, max };
+        return {p1, p2, max};
     };
 
 
