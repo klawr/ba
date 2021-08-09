5045a16c74ed16c6da2aa386d473c346792afe6a
Kai Lawrence <mail@kailawrence.de>
2021-08-09 14:37:29 +0200

    WIP find line by correlation

--- src/scripts/pointCloud.js
+++ src/scripts/pointCloud.js
@@ -124,6 +124,79 @@ class PointCloud {
         return new PointCloud(survivor);
     };
 
+    static correlation(pts) {
+        const len = pts.length;
+
+        const s = pts.reduce((pre, cur) => ({
+            x: pre.x + cur.x,
+            y: pre.y + cur.y,
+            xs: pre.xs + cur.x ** 2,
+            ys: pre.ys + cur.y ** 2,
+            xy: pre.xy + cur.x * cur.y,
+        }), { x: 0, y: 0, xs: 0, ys: 0, xy: 0 });
+
+
+        const nominator = s.xy - s.x * s.y;
+        const denominator = Math.sqrt(
+            (s.xs - (s.x ** 2 / len )) *
+            (s.ys - (s.y ** 2 / len)));
+        return nominator / denominator;
+    }
+
+    /**
+     * 
+     * @param {g2} g is a g2 command-queue used for rendering
+     * @param {number} bounds is the max distance of points
+     * @returns this PointCloud grouped up into an array of PointClouds 
+     */
+    groupByCorrelation(g, bounds = 10) {
+        const groups = [];
+        let ungrouped = [...this.points];
+
+        const min = ungrouped.length / 10;
+
+        const colors = ['#00f8', '#0f08', '#0ff8', '#f008', '#f0f8', '#ff08'];
+
+        let last = 0;
+        while (ungrouped.length > min && ungrouped.length != last) {
+            last = ungrouped.length;
+            // Get left most ungrouped point.
+            const pt = ungrouped.reduce((pre, cur) =>
+                pre.x < cur.x ? pre : cur);
+
+            // 36 lines are checked
+            const coefficients = [];
+
+            const lines = [];
+
+            for (let i = 0; i < 360; i += 10) {
+                const inbounds = [];
+
+                const m_i = Math.tan(i * Math.PI / 180)
+                const b_i = pt.y - m_i * pt.x;
+
+                lines.push({ m: m_i, b: b_i });
+
+                const m_o = - 1 / m_i;
+
+                ungrouped.forEach(e => {
+                    const b_o = e.y - m_o * e.x;
+
+                    const x = (b_o - b_i) / (m_i - m_o) || e.x;
+                    const y = m_o * x + b_o || b_i;
+
+                    if (Math.hypot(e.y - y, e.x - x) < bounds) {
+                        inbounds.push(e);
+                    }
+                });
+
+                coefficients.push(PointCloud.correlation(inbounds));
+            }
+
+            console.log(coefficients);
+        }
+    }
+
     /**
      * 
      * @param { g2 } g is a g2 command-queue used for rendering
@@ -142,16 +215,16 @@ class PointCloud {
 
         let last = 0;
         while (ungrouped.length > min && ungrouped.length != last) {
-            last = ungrouped.length
-            // const pt = ungrouped[0];
+            last = ungrouped.length;
+            // Get left most ungrouped point.
             const pt = ungrouped.reduce((pre, cur) =>
-                pre.left > cur.left ? pre : cur);
+                pre.x < cur.x ? pre : cur);
 
             const hypos = [];
             const lines = [];
 
             for (let i = 0; i < 360; i += 10) {
-                const m_i = Math.tan(i * Math.PI / 360);
+                const m_i = Math.tan(i * Math.PI / 180);
                 const b_i = pt.y - m_i * pt.x;
 
                 hypos.push([]);
@@ -170,18 +243,20 @@ class PointCloud {
                     const score = Math.pow(hypo, 1 / 3);
 
                     if (hypo < bounds) {
-                        fix++;
+                        fix += 2;
                     }
 
                     hypos[hypos.length - 1].push(score);
-
-                    // g.lin({ x1: x, y1: y, x2: e.x, y2: e.y });
                 });
                 hypos[hypos.length - 1] = hypos[hypos.length - 1].map(e => e / fix ** 3);
 
-                g && g.lin({ x1: 0, x2: 400, y1: b_i, y2: m_i * 400 + b_i, ls: 'lightgrey' });
+                g?.lin({
+                    x1: 0, x2: 400,
+                    y1: b_i, y2: m_i * 400 + b_i,
+                    ls: 'lightgrey'
+                });
             }
-            g && g.cir({ ...pt, r: 5, fs: 'green' });
+            g?.cir({ ...pt, r: 5, fs: 'green' });
 
             const score = hypos.map(h => h.reduce((pre, cur) => pre + cur));
 
