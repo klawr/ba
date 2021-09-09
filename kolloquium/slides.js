178d19c45f944b9bd5b03f6f2e662b99ba220eb0
klawr <mail@kailawrence.de>
2021-09-09 21:02:19 +0200

    Kolloquium - Update slides

--- kolloquium/slides.js
+++ kolloquium/slides.js
@@ -1,28 +1,34 @@
 const slides = {
-    mensch: {
-        title: "Was ist der Mensch?",
+    thema: {
+        title: 'Thema',
         slide: [
-            null
+            'Ziel',
+            'Aufteilung',
         ]
     },
-    wissen: {
-        title: "Was kann ich wissen?",
+    informationsgewinn: {
+        title: 'Informationsgewinn',
         slide: [
-            "Erkennen von Bewegung",
-            "Kleinster umfassender Kreis",
-            "Regressionsgeraden",
-            "Optischer Fluss",
-            "Shi-Tomasi",
-            "Lucas-Kanade"
+            'Erkennen von Bewegung',
+            'Kleinster umfassender Kreis',
+            'Regressionsgeraden',
+            'Optischer Fluss',
+            'Lucas-Kanade'
         ]
     },
-    tun: {
-        title: "Was kann ich tun?",
+    momentanpol: {
+        title: 'Momentanpol',
         slide: []
     },
-    hoffen: {
-        title: "Was kann ich hoffen?",
-        slide: []
+    gruppierung: {
+        title: 'Gruppierung',
+        slide: [],
+    },
+    zusammenfassung: {
+        title: 'Zusammenfassung',
+        slide: [
+            'Abbildungsverzeichnis'
+        ]
     }
 }
 
@@ -32,6 +38,17 @@ function addSlide(id) {
     div.className = 'slide';
 
     return div;
+}
+
+function createList(arr) {
+    const ol = document.createElement('ol');
+    arr.forEach((s) => {
+        const li = document.createElement('li');
+        li.innerHTML = s;
+        ol.appendChild(li);
+    });
+
+    return ol;
 }
 
 function createSlides(slideNumber) {
@@ -49,15 +66,12 @@ function createSlides(slideNumber) {
     document.body.appendChild(div);
     arr.push(div);
 
-    const ol = document.createElement('ol');
-    sl.slide.forEach((s, i) => {
-        const li = document.createElement('li');
-        li.innerHTML = s;
-        li.href = `#${i + 1}`;
-        ol.appendChild(li);
+    const ol = createList(sl.slide)
+
+    sl.slide.forEach((_, i) => {
         arr.push(createSlide(slideNumber, i + 1));
     });
-div.appendChild(ol);
+    div.appendChild(ol);
 
     return arr;
 }
@@ -77,4 +91,23 @@ function createSlide(slide, subSlide) {
     document.body.appendChild(div);
 
     return div;
+}
+
+function setIframe(parent, src, height, pre = '../../src/') {
+    const iframe = document.createElement('iframe');
+    iframe.src = pre + src;
+    iframe.style.height = '' + height + 'px';
+    parent.appendChild(iframe);
+}
+
+function setImage(parent, src, text, width, pre = '../../gfx/') {
+    const img = document.createElement('img');
+    img.src = pre + src;
+    img.width = width;
+    parent.appendChild(img);
+
+    const sub = document.createElement('h4');
+    sub.innerHTML = text;
+
+    parent.appendChild(sub);
 }
